4ef76004676e60e45e554d2ecb338dc4e611b8ff
Seth Green <seth127@gmail.com>
2016-09-19 15:50:09 -0400

    split mapping and loading

--- Austin-juris-map.R
+++ Austin-juris-map.R
@@ -3,97 +3,65 @@ library(ks)
 library(RColorBrewer)
 require(rgdal)
 library(MASS)
+require(dplyr)
 
-setwd("~/Documents/DSI/notes/2-SYS-6018/Case Study 1 - crime")
-source("load-Austin-data.R")
+source("CrimeUtil.R")
+
+#setwd("~/Documents/DSI/notes/2-SYS-6018/Case Study 1 - crime/SYS-6018-cs1")
 
 # LOAD THE DATA
-boundary <-readOGR(dsn="Neighborhood-Planning-Areas",layer="geo_export_bf249074-a2fc-4d35-b70e-d75de1175a06")
+# boundary <-readOGR(dsn="Neighborhood-Planning-Areas",layer="geo_export_bf249074-a2fc-4d35-b70e-d75de1175a06")
+# plot(boundary)
+
+boundary <-readOGR(dsn=".", layer="geo_export_7c52f690-58f6-4f4a-96fa-95df45d3770a")
+
+# This will draw the entire map of Austin. Available for export from: 
+# https://data.austintexas.gov/Government/Austin-Police-Sectors-and-Districts/bh6h-vpxb
+#boundary <-readOGR(dsn="Austin Police Sectors and Districts", layer="geo_export_7c52f690-58f6-4f4a-96fa-95df45d3770a")
+
 plot(boundary)
 
 stores <- read.csv("stores_info.csv", stringsAsFactors = F)
-plot(stores$Long, stores$Lat)
-#figure out how to plot this on top of boundary
+store_coords <- data.frame(x=stores$Long, y=stores$Lat)
+coordinates(store_coords) <- c('x','y')
+plot(store_coords, col="blue", add=T)
 
 crimes <- read.csv("Annual_Crime_Dataset_2015.csv", stringsAsFactors = F)
-# convert GO_X and GO_Y to lat and long, then plot on top of boundary
+crimes <- filter(crimes, GO.X.Coordinate != "") # remove crimes that have no location data
 
+coords <- data.frame(x=crimes$GO.X.Coordinate, y=crimes$GO.Y.Coordinate)
+coordinates(coords) <- c('x','y')
+proj4string(coords) <- CRS("+init=epsg:2277")
+coords_r <- spTransform(coords,CRS("+init=epsg:4326"))
 
-# get color palette for heat map
-palette.function = colorRampPalette(rev(brewer.pal(11,'Spectral')))
-heat.colors = palette.function(32)
+crimes$long <- coordinates(coords_r)[,1]
+crimes$lat <- coordinates(coords_r)[,2]
 
+# Reproject crime lon/lat points to meters
+crime.locations.lonlat = cbind(crimes$long, crimes$lat)
+crime.locations.meters = project(crime.locations.lonlat, proj="+init=epsg:26971")
 
-# EXPLORATORY PLOT THE DATA TO SEE WHAT WE'RE WORKING WITH
-plot(stores, pch = 20, col=stores$store)
-plot(boundary, add = TRUE)
-title("Cultural Hot Spots in Toronto")
-# legend("topright", legend = levels(td$CATEGORY), 
-#        col = td$CATEGORY) # can't get this to work for some reason
+# Reproject store location points to meters
+store.locations.lonlat = cbind(stores$Long, stores$Lat)
+store.locations.meters = project(store.locations.lonlat, proj="+init=epsg:26971")
 
-### OK, now do it for real
+# Create data frame from crime.locations.meters
+crime.locations <- as.data.frame(crime.locations.meters)
+names(crime.locations) <- c("x","y")
 
-# # seems like this should work, but it doesn't...
-# tdKDE1 = kde2d(td$LATITUDE, td$LONGITUDE, h=0.01, n=300)
-# image(tdKDE1, col = heat.colors, useRaster=TRUE, asp=1)
-# plot(boundary, add = TRUE)
-# points(td$LATITUDE, td$LONGITUDE, pch = 20, col="grey")
-# # so instead... we turn it into a data.frame?
+kde.sample.points = crime.locations[,c("x","y")]
 
-## subset out just the lat and long 
-tddf <- as.data.frame(td)
-td.sample.points <- tddf[ ,(ncol(tddf)-1):ncol(tddf)]
-names(td.sample.points) <- c("lat", "long")
+austin = spTransform(boundary, CRS(proj="+init=epsg:26971"))
 
-## NOW PLOT
-td1 = kde2d(td.sample.points$lat, td.sample.points$long, 
-             h=1, n=300, lims = tlims)
-var(c(td1$z))
-image(td1, col = heat.colors, useRaster=TRUE, asp=1)
-plot(boundary, add = TRUE)
-# h way too high
+# get estimation points for KDE
+kde.resolution.meters = 200
+kde.est.points = get.grid.points(austin, kde.resolution.meters, FALSE)
 
-td.1 = kde2d(td.sample.points$lat, td.sample.points$long, 
-             h=0.1, n=300, lims = tlims)
-var(c(td.1$z))
-image(td.1, col = heat.colors, useRaster=TRUE, asp=1)
-plot(boundary, add = TRUE)
-# h pretty good, but a little rough
+# run and plot KDE, using 500 points from the sample
+kde.est = run.spatial.kde(kde.sample.points, kde.est.points, 500) 
+plot.spatial.kde(kde.est, kde.est.points)
+plot(austin, add=T)
 
-td.01 = kde2d(td.sample.points$lat, td.sample.points$long, 
-             h=0.01, n=300, lims = tlims)
-var(c(td.01$z))
-image(td.01, col = heat.colors, useRaster=TRUE, asp=1)
-plot(boundary, add = TRUE)
-# h too fine
-
-td.05 = kde2d(td.sample.points$lat, td.sample.points$long, 
-             h=0.05, n=300, lims = tlims)
-var(c(td.05$z))
-image(td.05, col = heat.colors, useRaster=TRUE, asp=1)
-plot(boundary, add = TRUE)
-# goldilocks. That's a pretty good and descriptive fit.
-# the variance of the output is high, but not too high.
-
-#now plot the points in the sample data to compare
-points(td.sample.points$lat, td.sample.points$long, pch = 20, col="grey")
-# hard to say, this might be overfit, or it might be perfect
-##probably depends on your application
-
-# we're excited to have found this manually, but now we see what
-# Silverman's Rule of Thumb method gives us:
-bandwidth.nrd(td.sample.points$lat)
-#[1] 0.17677
-bandwidth.nrd(td.sample.points$long)
-#[1] 0.07473002
-##so we were pretty close on the longitude, but only semi-close on latitude
-
-# here is what it looks like with the "optimal" h values
-tdn = kde2d(td.sample.points$lat, td.sample.points$long, 
-              h=c(bandwidth.nrd(td.sample.points$lat), 
-                  bandwidth.nrd(td.sample.points$long)),
-              n=300, lims = tlims)
-image(tdn, col = heat.colors, useRaster=TRUE, asp=1)
-plot(boundary, add = TRUE)
-#and with points
-points(td.sample.points$lat, td.sample.points$long, pch = 20, col="grey")
+store_meters <- data.frame(x=store.locations.meters[,1], y=store.locations.meters[,2])
+coordinates(store_meters) <- c('x','y')
+plot(store_meters, col="red", add=T)
--- load-Austin-data.R
+++ load-Austin-data.R
@@ -1,70 +1,12 @@
 require(gdata)
-library(ks)
-library(RColorBrewer)
-require(rgdal)
-library(MASS)
-require(dplyr)
-
-source("CrimeUtil.R")
 
 #setwd("~/Documents/DSI/notes/2-SYS-6018/Case Study 1 - crime/SYS-6018-cs1")
 
 # LOAD THE DATA
-# boundary <-readOGR(dsn="Neighborhood-Planning-Areas",layer="geo_export_bf249074-a2fc-4d35-b70e-d75de1175a06")
-# plot(boundary)
-
-boundary <-readOGR(dsn=".", layer="geo_export_7c52f690-58f6-4f4a-96fa-95df45d3770a")
-
-# This will draw the entire map of Austin. Available for export from: 
-# https://data.austintexas.gov/Government/Austin-Police-Sectors-and-Districts/bh6h-vpxb
-#boundary <-readOGR(dsn="Austin Police Sectors and Districts", layer="geo_export_7c52f690-58f6-4f4a-96fa-95df45d3770a")
-
-plot(boundary)
-
 stores <- read.csv("stores_info.csv", stringsAsFactors = F)
-store_coords <- data.frame(x=stores$Long, y=stores$Lat)
-coordinates(store_coords) <- c('x','y')
-plot(store_coords, col="blue", add=T)
 
 crimes <- read.csv("Annual_Crime_Dataset_2015.csv", stringsAsFactors = F)
 crimes <- filter(crimes, GO.X.Coordinate != "") # remove crimes that have no location data
-
-coords <- data.frame(x=crimes$GO.X.Coordinate, y=crimes$GO.Y.Coordinate)
-coordinates(coords) <- c('x','y')
-proj4string(coords) <- CRS("+init=epsg:2277")
-coords_r <- spTransform(coords,CRS("+init=epsg:4326"))
-
-crimes$long <- coordinates(coords_r)[,1]
-crimes$lat <- coordinates(coords_r)[,2]
-
-# Reproject crime lon/lat points to meters
-crime.locations.lonlat = cbind(crimes$long, crimes$lat)
-crime.locations.meters = project(crime.locations.lonlat, proj="+init=epsg:26971")
-
-# Reproject store location points to meters
-store.locations.lonlat = cbind(stores$Long, stores$Lat)
-store.locations.meters = project(store.locations.lonlat, proj="+init=epsg:26971")
-
-# Create data frame from crime.locations.meters
-crime.locations <- as.data.frame(crime.locations.meters)
-names(crime.locations) <- c("x","y")
-
-kde.sample.points = crime.locations[,c("x","y")]
-
-austin = spTransform(boundary, CRS(proj="+init=epsg:26971"))
-
-# get estimation points for KDE
-kde.resolution.meters = 200
-kde.est.points = get.grid.points(austin, kde.resolution.meters, FALSE)
-
-# run and plot KDE, using 500 points from the sample
-kde.est = run.spatial.kde(kde.sample.points, kde.est.points, 500) 
-plot.spatial.kde(kde.est, kde.est.points)
-plot(austin, add=T)
-
-store_meters <- data.frame(x=store.locations.meters[,1], y=store.locations.meters[,2])
-coordinates(store_meters) <- c('x','y')
-plot(store_meters, col="red", add=T)
 
 ####
 
@@ -141,3 +83,4 @@ nrow(zeros) / nrow(stores)
 stores <- cbind(stores, storeCrimeCount$crimes)
 names(stores)[ncol(stores)] <- "crimes2014"
 head(stores)
+
